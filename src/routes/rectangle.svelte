219ae89166df682afbd59e286e9513f70061ca53
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-11-21 21:03:32 +0530

    d

--- src/routes/rectangle.svelte
+++ src/routes/rectangle.svelte
@@ -1,10 +1,29 @@
 <script defer>
   // @ts-nocheck
-
-  import "./styles.css";
   export let src;
   export let languageName;
   export let alt;
+  import { onMount } from "svelte";
+
+  import "./styles.css";
+
+  onMount(() => {
+    let grabElement = document.querySelectorAll(".rect");
+
+    const mouseMove = (e) => {
+      const { currentTarget: target } = e;
+      const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+      target.style.setProperty("--mouse-x", `${x}px`);
+      target.style.setProperty("--mouse-y", `${y}px`);
+    };
+
+    for (const rectGrad of grabElement) {
+      rectGrad.onmousemove = (e) => mouseMove(e);
+    }
+  });
 </script>
 
 <main>
@@ -18,16 +37,39 @@
 
 <style>
   .rect {
+    position: relative;
     width: 80%;
     height: 130%;
     display: flex;
     justify-content: center;
     align-items: center;
     background-color: rgba(0, 26, 81, 0.215);
-    border: 0.5px solid #727272;
+    border: 0.5px solid rgba(0, 12, 48, 1);
     border-radius: 5px;
     cursor: default;
   }
+
+  .rect:hover::before {
+    opacity: 1;
+  }
+  .rect::before {
+    position: absolute;
+    content: "";
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
+    z-index: -2;
+    opacity: 0;
+    transition: opacity 500ms;
+    background: radial-gradient(
+      400px circle at var(--mouse-x) var(--mouse-y),
+      rgba(145, 222, 255, 0.54),
+      transparent 40%
+    );
+  }
+
   .flexMain {
     width: 100%;
     text-align: center;
